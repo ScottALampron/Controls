e0e469320114dd169ec3641ef141bf9add769a9c
ScottALampron <105624697+ScottALampron@users.noreply.github.com>
2024-02-23 11:45:29 -0800

    Create vars.py

---
+++ documentation_made_simple/vars.py
@@ -0,0 +1,7 @@
+# vars.py
+
+# GitHub repository details
+github_owner = 'owner_username'
+github_repo = 'repository_name'
+# Enable/disable installation of requirements
+enable_requirements_installation = True
