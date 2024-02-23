76ee8c8e7c7ab421dd92d2a115b256af666f7420
ScottALampron <105624697+ScottALampron@users.noreply.github.com>
2024-02-23 11:49:33 -0800

    Update vars.py

--- documentation_made_simple/vars.py
+++ documentation_made_simple/vars.py
@@ -5,3 +5,5 @@ github_owner = 'owner_username'
 github_repo = 'repository_name'
 # Enable/disable installation of requirements
 enable_requirements_installation = True
+# Path to requirements file
+requirements_file_path = 'requirements.txt'
