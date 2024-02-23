e47308c4c869cf6279e9f253a1fb8c7c6ffca7af
ScottALampron <105624697+ScottALampron@users.noreply.github.com>
2024-02-23 12:07:10 -0800

    Create vars.py

---
+++ documentation_made_simple/ai_assisted/vars.py
@@ -0,0 +1,24 @@
+# vars.py
+# OpenAI API key
+openai_api_key = 'your_openai_api_key'
+# Prompt for generating README content
+readme_prompt = """
+# Project Name
+
+## Introduction
+
+## Features
+
+## Installation
+
+## Usage
+
+## Contributing
+
+## License
+"""
+
+# Path to requirements file
+requirements_file_path = 'requirements.txt'
+# Enable/disable installation of requirements
+enable_requirements_installation = True
